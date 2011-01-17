e596e7721f14d5e19149f2a563b7a23bfb048676
Alex Henning <elcron@gmail.com>
2011-01-16 19:08:12 -0500

    Added initial support for drag and drop, no major bugs that I'm aware of.

--- scoring.js
+++ scoring.js
@@ -26,9 +26,9 @@ function Peg(raised, parent) {
 
     var obj = this;
     this.html.click(function() { obj.clickHandler(); });
-    this.html.rightClick(function() { obj.rightClickHandler(); });
-    this.html.bind("touchstart", function(e) { obj.touchStart(e); });
-    this.html.bind("touchend", function(e) { obj.touchEnd(e); });
+    // this.html.rightClick(function() { obj.rightClickHandler(); });
+    // this.html.bind("touchstart", function(e) { obj.touchStart(e); });
+    // this.html.bind("touchend", function(e) { obj.touchEnd(e); });
 }
 Peg.prototype.clickHandler = function() {
     this.tube += 1;
@@ -58,12 +58,82 @@ Peg.prototype.touchEnd = function(e) {
 	this.clickHandler();
     }
 };
+// Detection of overlap of a point for ubertubes
+Peg.prototype.contains = function(px, py) {
+    var x, y, w, h;
+    x = this.html.attr("offsetLeft");
+    y = this.html.attr("offsetTop");
+    w = this.html.attr("clientWidth");
+    h = this.html.attr("clientHeight");
+    return ((x <= px) && (px <= (x+w))) && ((y <= py) && (py <= (y+h)));
+};
 
 // Generates a row of pegs
 function getRow(parent) {
     return [new Peg(false, parent), new Peg(true, parent), new Peg(false, parent),
 	    new Peg(false, parent), new Peg(true, parent), new Peg(false, parent)];
 }
+
+/**
+ * Code for dragging and dropping of UberTubes
+ */
+var ubertubes;
+var numUBERTUBEs = 0;
+function UberTube() {
+    this.num = numUBERTUBEs; numUBERTUBEs += 1;
+    this.html = $("<img src='uber_none.png' />");
+    this.html.appendTo($("#bin"));
+    this.html.toggleClass("inBin");
+    this.clicked = false;
+    this.container = $("#bin");
+
+    var obj = this;
+    this.html.mousedown( function(e) {obj.handleMouseDown(e);} );
+    this.html.mousemove( function(e) {obj.handleMouseMove(e);} );
+    this.html.mouseup( function(e) {obj.handleMouseUp(e);} );
+}
+UberTube.prototype.handleMouseDown = function(e) {
+    e.preventDefault();
+    this.clicked = true;
+    this.html.removeClass("inBin");
+    this.html.toggleClass("movingTube");
+    this.html.attr("src", "uber_none.png");
+    this.html.css("top", (e.pageY-45)+"px");
+    this.html.css("left", (e.pageX-45)+"px");
+    if (this.container instanceof Peg) {
+	this.container.rightClickHandler();
+    }
+};
+UberTube.prototype.handleMouseMove = function(e) {
+    if (this.clicked) {
+	e.preventDefault();
+	this.html.css("top", (e.pageY-45)+"px");
+	this.html.css("left", (e.pageX-45)+"px");
+    }
+};
+UberTube.prototype.handleMouseUp = function(e) {
+    e.preventDefault();
+    this.clicked = false;
+    this.html.toggleClass("movingTube");
+    this.html.css("position", "absolute");
+
+    var handled = false;
+    for (var row = 0; row < grid.length; row++) {
+	for (var i = 0; i < grid[row].length; i++) {
+	    if (grid[row][i].contains(e.pageX, e.pageY)) {
+		grid[row][i].rightClickHandler();
+		this.container = grid[row][i];
+		this.html.css("left", grid[row][i].html.attr("offsetLeft"));
+		this.html.css("top", grid[row][i].html.attr("offsetTop"));
+		this.html.attr("src", "blank.png");
+		handled = true;
+	    }
+	}
+    }
+    if (!handled) {
+	this.container = $("#bin");
+    }
+};
 
 /**
  * Code for the minibots race
@@ -160,4 +230,5 @@ function main() {
 	    new Racer(RACE_POSITION[1], RACE_SCORE[1]),
 	    new Racer(RACE_POSITION[2], RACE_SCORE[2]),
 	    new Racer(RACE_POSITION[3], RACE_SCORE[3])];
+    ubertubes = [new UberTube(), new UberTube(),  new UberTube()];
 }
